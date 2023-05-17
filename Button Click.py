b5c7854eaaaa3ae54d337d2cb9c7b710ec8b0a03
Kibrom Hagos <royalkibrom@gmail.com>
2023-05-17 09:56:02 -0700

    update

--- Button Click.py
+++ Button Click.py
@@ -1,14 +1,16 @@
-#
+#import tinker library
 import tkinter as tk
-#
+
+#declaring the function
 def button_click():
     print("Button Clicked")
-#
+
+#Create the root window
 root=tk.Tk()
 root.title("Button Example")    
 
 
-#
+#create the button object, including the 3 argument 
 button = tk.Button(root, text="Click Me!", command=button_click)
 button.pack()
 
